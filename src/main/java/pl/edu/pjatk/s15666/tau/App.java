5c1e6bae3ee5a7eacd9dc063563a5d9aa9ae0c38
Tomasz Szczodrowski <bdn583@gmail.com>
2019-10-06 10:08:31 +0200

    Generate maven project

---
+++ src/main/java/pl/edu/pjatk/s15666/tau/App.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.s15666.tau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
