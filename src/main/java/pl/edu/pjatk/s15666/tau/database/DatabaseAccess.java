7bcfa83d8243a8ad1fc762772cff1be1ec06ce54
Tomasz Szczodrowski <bdn583@gmail.com>
2019-10-06 10:22:49 +0200

    Add DatabaseAccess

---
+++ src/main/java/pl/edu/pjatk/s15666/tau/database/DatabaseAccess.java
@@ -0,0 +1,33 @@
+package pl.edu.pjatk.s15666.tau.database;
+
+import com.sun.tools.javac.util.List;
+
+public class DatabaseAccess {
+
+    private List<Object> db;
+
+    public DatabaseAccess(List<Object> db) {
+        this.db = db;
+    }
+
+    public void create(Object o) {
+
+    }
+
+    public List<Object> readAll() {
+        return null;
+    }
+
+    public Object read() {
+        return null;
+    }
+
+    public void update(Object o) {
+
+    }
+
+    public void delete(Object o) {
+
+    }
+
+}
